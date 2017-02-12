0a0428cbd75ce6568cc4d817876a5d910b01de12
migmosquera <migmosquera@hotmail.com>
2017-02-12 13:31:06 -0400

    subiendo

---
+++ contactenos.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<?php
+	
+?>
+<html lang="en">
+
+<head>
+<title>MPITCA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- bootstrap-css -->
+    <link href="static/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <!--// bootstrap-css -->
+    <!-- css -->
+    <link rel="stylesheet" href="static/css/style.css" type="text/css" media="all" />
+    <!--// css -->
+    <!-- font-awesome icons -->
+    <link href="static/css/font-awesome.css" rel="stylesheet">
+    <script src="static/js/jquery-1.11.1.min.js"></script>
+    <script src="static/js/bootstrap.js"></script>
+    <!-- light-box -->
+    <script src="static/js/SmoothScroll.min.js"></script>
+    <link rel="stylesheet" href="static/css/aos.css" type="text/css" media="all" />
+</head>
+<body style="background-color: #133F54">
+	<!-- banner -->
+	<div class="banner banner_interno">
+		<div class="header">
+			<div class="container container_header">
+				<div class="header-left">
+					<div class="w3layouts-logo">
+						<h1>
+                                                    <a class="logo_web" href="index.php"><img src="static/images/mpitca.png"  /></a>
+                                                        <a class="logo_mobil" href="index.php"><img src="static/images/mpitca_mobile.png"  /></a>
+						</h1>
+					</div>
+					<!--<div class="agileinfo-social-grids">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						</ul>
+					</div>-->
+					<div class="clearfix"> </div>
+				</div>
+				<div class="top-nav">
+					<nav class="navbar navbar-default">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a  href="index.php">Home</a></li>
+								<li><a  href="servicios.php" class="scroll">Servicios</a></li>
+								<li><a href="" class="scroll">Beneficios</a></li>
+								<li><a href="" class="scroll">Sobre Nosotros</a></li>	
+								<li><a href="" class="scroll">Poyectos Realizados</a></li>
+								<li><a class="active" href="contactenos.php" class="scroll">Contactenos</a></li>
+							</ul>
+							<div class="clearfix"> </div>							
+						</div>	
+					</nav>		
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		
+	</div>
+	
+	<div style="background-color: #133F54" >
+		<div class="container_servicios">
+			<div style="display: block">
+				<div class="linea_degradada_izquierda"></div>
+				<h3 class="title_serivicios"  >Contactenos</h3>	
+				<div class="linea_degradada_derecha"></div>
+			</div>
+			<div style="text-align: center; width: 100%; padding-bottom: 30px;">
+				<p><strong>Mejores Prácticas en Tecnología de Información, C.A.</strong></p>
+				<p><strong>MPITCA</strong></p>
+				<p>Urb. El Naranjal 2 Calle 198-A, Casa 120-92</p>
+				<p>Naguanagua - Valencia - Edo. Carabobo 2005</p>
+				<p>Venezuela</p>
+				<div style="display: block">
+					<img style="display: inline-block" src="static/images/fax-top-view.png" />
+					<p style="display: inline-block">58-241-8674543</p>
+				</div>
+				<div style="display: block">
+					<button type="button" data-toggle="modal" data-target="#myModalMpitca" style="border: none;background: none;" >
+						<img style="display: inline-block" src="static/images/envelope.png" />
+						<p style="display: inline-block">mpit@mpitca.com.ve</p>
+					</button>
+				</div>
+			</div>
+			<div style="text-align: center; width: 49%; display: inline-block;padding-bottom: 30px;">
+				<p><strong>Charles Chaderton</strong></p>
+				<p>CIO - Chief Information Officer</p>
+				
+				<div style="display: block">
+					<img style="display: inline-block" src="static/images/smartphone-call.png" />
+					<p style="display: inline-block">58-414-4225146</p>
+				</div>
+				<div style="display: block">
+					<button type="button" data-toggle="modal" data-target="#myModalCharles" style="border: none;background: none;" >
+						<img style="display: inline-block" src="static/images/envelope.png" />
+						<p style="display: inline-block">charles.chaderton@mpitca.com.ve</p>
+					</button>
+				</div>
+			</div>
+			<div style="text-align: center; width: 49%; display: inline-block;padding-bottom: 30px;">
+				<p><strong>Jorge Mosquera</strong></p>
+				<p>CDO - Chief Information Officer</p>
+				
+				<div style="display: block">
+					<img style="display: inline-block" src="static/images/smartphone-call.png" />
+					<p style="display: inline-block">58-424-4460910</p>
+				</div>
+				<div style="display: block">
+					<button type="button" data-toggle="modal" data-target="#myModalJormos" style="border: none;background: none;" >
+						<img style="display: inline-block" src="static/images/envelope.png" />
+						<p style="display: inline-block">jorge.mosquera@mpitca.com.ve</p>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<!-- footer -->
+	<div class="footer" style="margin-top: 25px;" >
+		<div class="footer-top">
+			<div class="container">
+				<div class="footer-grids" style="text-align: center; display: block;margin: 0 auto;">
+					<div class=" footer-grid" style="display: inline-block; width: 32%;float: left;">
+						<div class="footer-logo">
+							<h3><a href="index.php">MPIT</a></h3>
+						</div>
+					</div>
+					<div class=" footer-list" style="display: inline-block; width: 32%;float: left; text-align: left" >
+						<h3>Servicios</h3>
+						<ul>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Usuario Final</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Escritorio de Ayuda</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Soporte de Sitio</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Mantenimiento de Hardware</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Soporte de Infraestructura</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Gerencia del Ciclo de Vida de su Tecnología</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Seguridad</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Consulta y Proyecto</a></li>
+						</ul>
+					</div>
+					<div class="agile-footer-grid" style="display: inline-block; width: 32%;float: left;">
+						<h3>Contactenos</h3>
+						<div id="contJormos" style="color: #A7A5A5; cursor: pointer; margin-top: 15px;" ><a href="contactenos.php" style="color: #c0c0c0;;border: none;background: none;" >Jorge Mosquera</a></div>
+						<div id="contCharles" style="color: #A7A5A5; cursor: pointer; margin-top: 15px;"><a href="contactenos.php" style="color: #c0c0c0;;border: none;background: none;" >Charles Chaderton</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="myModalJormos" role="dialog">
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Jorge Mosquera</h4>
+	        </div>
+	        <div class="modal-body">
+	        	<div style="display: block">
+	        			<p  style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Para:</strong></p>
+	        			<p style="display: inline-block; color: #333333">Jorge Mosquera</p>
+	        		</div>
+	        		<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Asunto:</strong></p>
+	        			<p style="display: inline-block; color: #333333">Consulta</p>
+	        		</div>
+	        		<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>De:</strong></p>
+	        			<input style="width: 55%" type="text" placeholder="De:" value="" />
+	        		</div>
+		          	<div style="display: block; margin-bottom: 13px;">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Email:</strong></p>
+	        			<input style="width: 55%" type="text" placeholder="email:" value="" />
+	        		</div>
+		          	<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; vertical-align: top;"><strong>Contenido:</strong></p>
+	        			<textarea style="width: 55%;height: 100px;" placeholder="Contenido"  ></textarea>
+	        		</div>
+	          
+	        </div>
+	        <div class="modal-footer">
+	        	<input type="button" value="Enviar" />	
+	          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        </div>
+	      </div>
+	      
+	    </div>
+	  </div>
+	  <div class="modal fade" id="myModalCharles" role="dialog">
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Charles Chaderton</h4>
+	        </div>
+	        <div class="modal-body">
+	        	<div style="display: block">
+	        			<p  style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Para:</strong></p>
+	        			<p style="display: inline-block; color: #333333">Charles Chaderton</p>
+	        		</div>
+	        		<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Asunto:</strong></p>
+	        			<p style="display: inline-block; color: #333333">Consulta</p>
+	        		</div>
+	        		<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>De:</strong></p>
+	        			<input style="width: 55%" type="text" placeholder="De:" value="" />
+	        		</div>
+		          	<div style="display: block; margin-bottom: 13px;">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Email:</strong></p>
+	        			<input style="width: 55%" type="text" placeholder="email:" value="" />
+	        		</div>
+		          	<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; vertical-align: top;"><strong>Contenido:</strong></p>
+	        			<textarea style="width: 55%;height: 100px;" placeholder="Contenido"  ></textarea>
+	        		</div>
+	          
+	        </div>
+	        <div class="modal-footer">
+	        	<input type="button" value="Enviar" />	
+	          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        </div>
+	      </div>
+	      
+	    </div>
+	  </div>
+	  <div class="modal fade" id="myModalMpitca" role="dialog">
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">MPITCA</h4>
+	        </div>
+	        <div class="modal-body">
+	        	
+	        		<div style="display: block">
+	        			<p  style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Para:</strong></p>
+	        			<p style="display: inline-block; color: #333333">MPITCA</p>
+	        		</div>
+	        		<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Asunto:</strong></p>
+	        			<p style="display: inline-block; color: #333333">Consulta</p>
+	        		</div>
+	        		<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>De:</strong></p>
+	        			<input style="width: 55%" type="text" placeholder="De:" value="" />
+	        		</div>
+		          	<div style="display: block; margin-bottom: 13px;">
+	        			<p style="display: inline-block; color: #333333;width: 15%; text-align: end;"><strong>Email:</strong></p>
+	        			<input style="width: 55%" type="text" placeholder="email:" value="" />
+	        		</div>
+		          	<div style="display: block">
+	        			<p style="display: inline-block; color: #333333;width: 15%; vertical-align: top;"><strong>Contenido:</strong></p>
+	        			<textarea style="width: 55%;height: 100px;" placeholder="Contenido"  ></textarea>
+	        		</div>
+		          	
+	          	
+	          	
+	          
+	        </div>
+	        <div class="modal-footer">
+	        	<input type="button" value="Enviar" />	
+	          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        </div>
+	      </div>
+	      
+	    </div>
+	  </div>
+	<!-- //footer -->
+	<!--<div class="copyright">
+		<div class="container">
+			<p>© 2017 Swim Care. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+		</div>
+	</div>-->
+<script src="static/js/responsiveslides.min.js"></script>
+
+
+<script type="text/javascript" src="static/js/move-top.js"></script>
+<script type="text/javascript" src="static/js/easing.js"></script>
+<script type="text/javascript" src="static/js/aos.js"></script>
+</script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+</body>	
+</html>
---
+++ servicios.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<?php
+	
+?>
+<html lang="en">
+
+<head>
+<title>MPITCA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- bootstrap-css -->
+    <link href="static/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <!--// bootstrap-css -->
+    <!-- css -->
+    <link rel="stylesheet" href="static/css/style.css" type="text/css" media="all" />
+    <!--// css -->
+    <!-- font-awesome icons -->
+    <link href="static/css/font-awesome.css" rel="stylesheet">
+    <script src="static/js/jquery-1.11.1.min.js"></script>
+    <script src="static/js/bootstrap.js"></script>
+    <!-- light-box -->
+    <script src="static/js/SmoothScroll.min.js"></script>
+    <link rel="stylesheet" href="static/css/aos.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="static/css/servicios.css" type="text/css" media="all" />
+</head>
+<body style="background-color: #133F54">
+	<!-- banner -->
+	<div class="banner banner_interno" >
+		<div class="header">
+			<div class="container container_header">
+				<div class="header-left">
+					<div class="w3layouts-logo">
+						<h1>
+                                                    <a class="logo_web" href="index.php"><img src="static/images/mpitca.png"  /></a>
+                                                        <a class="logo_mobil" href="index.php"><img src="static/images/mpitca_mobile.png"  /></a>
+						</h1>
+					</div>
+					<!--<div class="agileinfo-social-grids">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						</ul>
+					</div>-->
+					<div class="clearfix"> </div>
+				</div>
+				<div class="top-nav">
+					<nav class="navbar navbar-default">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a  href="index.php">Home</a></li>
+								<li><a class="active" href="servicios.php" class="scroll">Servicios</a></li>
+								<li><a href="" class="scroll">Beneficios</a></li>
+								<li><a href="" class="scroll">Sobre Nosotros</a></li>	
+								<li><a href="" class="scroll">Poyectos Realizados</a></li>
+								<li><a href="contactenos.php" class="scroll">Contactenos</a></li>
+							</ul>
+							<div class="clearfix"> </div>							
+						</div>	
+					</nav>		
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		
+	</div>
+	<div id="containerServices" style="background-color: #133F54" >
+		<div id="subContainerServices" class="container_servicios">
+			<div style="display: block">
+				<div class="linea_degradada_izquierda"></div>
+				<h3 class="title_serivicios"  >Nuestro Servicios</h3>	
+				<div class="linea_degradada_derecha"></div>
+			</div>
+			<div id="servicesUser" class="container_content_services" >
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')" >Servicio de Usuario Final</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					<strong>MPIT</strong> 
+	              	estandariza el ambiente de escritorio para los usuarios finales. 
+	              	De esta forma, todos sus usuarios se sentir&aacute;n c&oacute;modos 
+	              	ya que no importa en qu&eacute; departamento de su compa&ntilde;&iacute;a 
+	              	se ubiquen, su ambiente de escritorio ser&aacute; el mismo, adem&aacute;s 
+	              	de minimizar sus costos al momento de entrenar a su personal en 
+	              	el uso de la infraestructura de escritorio, al agregar una nueva 
+	              	pc o utilizar nuestro servicio de Escritorio de ayuda.
+				</div>
+			</div>
+			<div id="servicesHelp" class="container_content_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Escritorio de Ayuda</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					Obtenga la ayuda que usted necesita r&aacute;pido. Llame a nuestro personal 
+	              	t&eacute;cnico directamente para hacer preguntas y obtener resoluci&oacute;n 
+	              	de problemas t&eacute;cnicos y hacer requerimientos de servicio.
+				</div>
+			</div>
+			<div id="servicesSupport"  class="container_content_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Soporte en Sitio</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					Cuando nuestro cliente necesita soporte en sitio, nuestro servicio de campo 
+	              	estar&aacute; all&iacute; para ayudarlo. <strong>MPIT</strong> provee 
+	              	de implementaci&oacute;n y mantenimiento de su infraestructura de 
+	              	PC&#8217;s de escritorio, incluyendo problemas de hardware (break/fix), 
+	              	instalaciones, movilizaciones, nuevos equipos, cambios y soporte 
+	              	de software de escritorio.
+				</div>
+			</div>
+			<div id="servicesHardware" class="container_content_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Mantenimiento de Hardwar</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					<strong>MPIT</strong> le prepara los planes de mantenimiento preventivo y correctivo de 
+	              	sus activos de inform&aacute;tica (PC&#8217;s y sus perif&eacute;ricos 
+	              	-impresoras, teclados, scanners, etc.-).
+				</div>
+			</div>
+			<div id="servicesInsfrastructure" class="container_content_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Soporte en Infraestructura</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					Minimice los riesgos de su infraestructura de escritorio. <strong>MPIT</strong> 
+	              	mantiene y gerencia su red de &aacute;rea local (LAN) incluyendo 
+	              	su conexi&oacute;n WAN, utilizando las mejores pr&aacute;cticas 
+	              	en gerencia de redes y diferentes paquetes de gerencia de redes 
+	              	que se amoldan a su presupuesto, en los cuales se definen niveles 
+	              	de rendimiento y disponibilidad. Este soporte tambi&eacute;n incluye 
+	              	mantenimiento y gerencia de sus servidores.
+				</div>
+			</div>
+			<div id="servicesCiclo" class="container_content_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Gerencia del Ciclo de Vida de su Tecnología</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					Conc&eacute;ntrese solo en su negocio medular, <strong>MPIT</strong> le llevar&aacute; 
+	          		el control de inventario de su infraestructura de tecnolog&iacute;a, 
+	          		tambi&eacute;n le puede hacer y llevar control de sus procuras en el 
+	          		&aacute;rea de tecnolog&iacute;a.
+				</div>
+			</div>
+			<div id="servicesAntivirus" class="container_content_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Gerencia de Antivirus</h4>
+				<div style="text-align: justify; font-size: 15px; margin-bottom: 15px;">
+					Hoy día, exite una creencia que al tener antivirus en los desktops y un firewall, la entidad está 
+					protegida, y aunque hay seguridad en la gerencia de los ítems mencionados anteriormente, seguridad empresarial
+					va mas allá, en realidad se requiere una combinación de ambos, la utilización de mejores prácticas y el esfuerzo 
+					cambinado y cooperación de todo el personal en la organización.
+				</div>	
+				<div style="text-align: justify; font-size: 15px;">
+					Prevenci&oacute;n es la clave, ya que es m&aacute;s costoso limpiar los virus despu&eacute;s 
+	         	 	que ya han contaminado su plataforma. Las metas de la gerencia de antivirus 
+	          		es reducir el tiempo sin servicios y gastos. Las mejores pr&aacute;cticas 
+	          		incluyen firewalls y servidores proxys, paquetes de antivirus, paquetes 
+	          		de antivirus para correo electr&oacute;nico, sistemas de respaldo empresariales.
+				</div>
+			</div>
+			<div id="servicesConsult" class="container_content_services finish_services">
+				<h4 class="container_title_services" style="background-image: url('static/images/fondo.png')">Servicio de Consultoría y Proyectos</h4>
+				<div style="text-align: justify; font-size: 15px;">
+					<strong>MPIT</strong> sirve a su empresa en la definici&oacute;n de arquitecturas hardware/software, 
+	          		selecci&oacute;n e implantaci&oacute;n de metodolog&iacute;as, planes 
+	          		de sistemas y estrategias de transformaci&oacute;n tecnol&oacute;gica. 
+	          		Nuestro objetivo es ayudarle a mejorar los procesos de informaci&oacute;n 
+	          		de su empresa; asesorando, dise&ntilde;ando y desarrollando planes de 
+	          		innovaci&oacute;n tecnol&oacute;gicos.
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	<!-- footer -->
+	<div class="footer" style="margin-top: 25px;" >
+		<div class="footer-top">
+			<div class="container">
+				<div class="footer-grids" style="text-align: center; display: block;margin: 0 auto;">
+					<div class=" footer-grid" style="display: inline-block; width: 32%;float: left;">
+						<div class="footer-logo">
+							<h3><a href="index.php">MPIT</a></h3>
+						</div>
+					</div>
+					<div class=" footer-list" style="display: inline-block; width: 32%;float: left; text-align: left" >
+						<h3>Servicios</h3>
+						<ul>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Usuario Final</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Escritorio de Ayuda</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Soporte de Sitio</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Mantenimiento de Hardware</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Soporte de Infraestructura</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Gerencia del Ciclo de Vida de su Tecnología</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Seguridad</a></li>
+							<li style="font-size: 11px;"><i class="fa fa-arrow-right" aria-hidden="true"></i><a>Servicio de Consulta y Proyecto</a></li>
+						</ul>
+					</div>
+					<div class="agile-footer-grid" style="display: inline-block; width: 32%;float: left;">
+						<h3>Contactenos</h3>
+						<div id="contJormos" style="color: #A7A5A5; cursor: pointer; margin-top: 15px;" ><a href="contactenos.php" style="color: #c0c0c0;;border: none;background: none;" >Jorge Mosquera</a></div>
+						<div id="contCharles" style="color: #A7A5A5; cursor: pointer; margin-top: 15px;"><a href="contactenos.php" style="color: #c0c0c0;;border: none;background: none;" >Charles Chaderton</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- //footer -->
+	<!--<div class="copyright">
+		<div class="container">
+			<p>© 2017 Swim Care. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+		</div>
+	</div>-->
+<script src="static/js/responsiveslides.min.js"></script>
+
+
+<script type="text/javascript" src="static/js/move-top.js"></script>
+<script type="text/javascript" src="static/js/easing.js"></script>
+<script type="text/javascript" src="static/js/aos.js"></script>
+</script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+</body>	
+</html>
